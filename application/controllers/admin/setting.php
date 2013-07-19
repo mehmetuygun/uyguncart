3c14ffd49b410e8c3679b32254de8475d512b032
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-07-19 20:27:26 +0300

    password

--- application/controllers/admin/setting.php
+++ application/controllers/admin/setting.php
@@ -17,7 +17,7 @@ class Setting extends CI_Controller {
 		$data["base_url"] = $this->load->helper(array('form', 'url'));
 		$data["title"] = "UygunCart";
 		$data["breadcrumb"] = array("admin/home"=>"Dashboard","admin/setting"=>"Setting","last"=>"Account");
-		$data["fullname"] = $this->session->userdata('userFirstName').' '.$this->session->userdata('userLastName');
+		
 		$data["mainview"] = "account";
 
 		$data["js"] = array("public/js/ajax.js");
@@ -38,17 +38,17 @@ class Setting extends CI_Controller {
 			}
 			else if($this->input->post('lname')!=$this->Setting_model->userLastName){
 				$this->form_validation->set_rules('lname','Last Name','required|min_length[3]|max_length[45]|alpha');
-				$field[] = array('userLastName'=>$this->input->post('userLastName'));
+				$field[] = array('userLastName'=>$this->input->post('lname'));
 			}
 			else{
-				$data["alert_message"] = "No data entered.";
+				$data["alert_message"] = "Enter data you want to update.";
 				$data["alert_class"] = "alert-error";
 			}
 
 			$field = array_shift($field);
 			if(!is_null($field))
 			{
-				$this->form_validation->set_rules('pwd','Password','required|min_length[3]|max_length[45]|checkpassword');
+				$this->form_validation->set_rules('pwd','Password','required|min_length[8]|max_length[64]|checkpassword');
 			}
 
 			$this->form_validation->set_error_delimiters('', '');
@@ -60,6 +60,7 @@ class Setting extends CI_Controller {
 					$data["alert_message"] = "Your data is update succesfuly.";
 					$data["alert_class"] = "alert-success";
 					$this->Setting_model->set($this->session->userdata('userID'));
+					$this->session->set_userdata($field);
 				}
 				else
 				{
@@ -73,8 +74,37 @@ class Setting extends CI_Controller {
 		$data["userFirstName"] = $this->Setting_model->userFirstName;
 		$data["userLastName"] = $this->Setting_model->userLastName;
 
+		$data["fullname"] = $this->session->userdata('userFirstName').' '.$this->session->userdata('userLastName');
 		$this->load->view('admin/default',$data);
 
 		$this->User_model->admin_logged();
+	}
+
+	public function password(){
+		$this->load->model('User_model');
+		$this->load->model('Setting_model');
+		$this->load->library('session');
+		$this->load->library('form_validation');
+
+		$data["base_url"] = $this->load->helper(array('form', 'url'));
+		$data["title"] = "UygunCart";
+		$data["breadcrumb"] = array("admin/home"=>"Dashboard","admin/setting"=>"Setting","last"=>"Password");
+		$data["fullname"] = $this->session->userdata('userFirstName').' '.$this->session->userdata('userLastName');
+		$data["mainview"] = "password";
+
+		$this->form_validation->set_rules('current_password','Current Password','required|min_length[8]|max_length[64]|checkpassword');
+		$this->form_validation->set_rules('new_password','New Password','required|min_length[8]|max_length[64]|matches[confirm_password]');
+		$this->form_validation->set_rules('confirm_password','Confirm Password','required|min_length[8]|max_length[64]|matches[new_password]');
+
+		if($this->form_validation->run() == TRUE)
+		{
+			if($this->Setting_model->update_account(array('userPassword'=>$this->input->post('new_password')),$this->session->userdata('userID')))
+			{
+				$data["alert_message"] = "Your data is update succesfuly.";
+				$data["alert_class"] = "alert-success";
+			}
+		}
+
+		$this->load->view('admin/default',$data);
 	}
 }
