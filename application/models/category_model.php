60145c3d3c73a1c55d40e3a68a90679ec3f69880
Omer Karadagli <ufokaradagli@gmail.com>
2013-07-24 22:20:17 +0100

    More improvements to category model

--- application/models/category_model.php
+++ application/models/category_model.php
@@ -79,7 +79,7 @@ class Category_model extends CI_model
 	{
 		$this->load->database();
 		$this->db->from('category')->where('categoryID', $categoryID);
-		if($this->db->count_all_results()>0)
+		if ($this->db->count_all_results() > 0)
 			return true;
 		return false;
 	}
@@ -96,14 +96,14 @@ class Category_model extends CI_model
 		if (empty($id)) return '';
 
 		$this->load->database();
-		$this->db->from('category')->where('categoryID', $id);
+		$this->db->from('category')
+			->where('categoryID', $id);
 
-		$query = $this->db->get();
-		$row = $query->result_array();
+		$row = $this->db->get()->row();
 		if (empty($row)) return '';
 
-		return $this->get_path($row[0]['parentID'], ' / ')
-			. $row[0]['categoryName'] . $sep;
+		return $this->get_path($row->parentID, ' / ')
+			. $row->categoryName . $sep;
 	}
 
 	/**
@@ -159,7 +159,7 @@ class Category_model extends CI_model
 
 		$query = $this->db->get();
 		$field = array();
-		foreach ($query->result() as  $row) {
+		foreach ($query->result() as $row) {
 			$field[] = array(
 				'categoryID' => $row->categoryID,
 				'categoryName' => $this->get_path($row->categoryID),
