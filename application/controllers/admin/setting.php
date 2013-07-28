c2d0c87b0ef69891975927ff58f89e1f604c8bb4
Omer Karadagli <ufokaradagli@gmail.com>
2013-07-28 16:28:46 +0100

    Tidy up setting controller, fix category error color.

--- application/controllers/admin/setting.php
+++ application/controllers/admin/setting.php
@@ -7,6 +7,7 @@ class Setting extends CI_Controller {
 		$this->load->helper('url');
 		redirect('/admin/setting/account', 'location', 301);
 	}
+
 	public function account()
 	{
 		$this->load->model('User_model');
@@ -14,58 +15,84 @@ class Setting extends CI_Controller {
 		$this->load->library('session');
 		$this->load->library('form_validation');
 
-		$data["base_url"] = $this->load->helper(array('form', 'url'));
-		$data["title"] = "UygunCart";
-		$data["breadcrumb"] = array("admin/home"=>"Dashboard","admin/setting"=>"Setting","last"=>"Account");
+		$data = array(
+			'base_url' => $this->load->helper(array('form', 'url')),
+			'title' => 'UygunCart',
+			'breadcrumb' => array(
+				'admin/home' => 'Dashboard',
+				'admin/setting' => 'Setting',
+				'last' => 'Account'
+			),
+			'mainview' => 'account'
+		);
 		
-		$data["mainview"] = "account";
-
-		$data["js"] = array("public/js/ajax.js");
-		
-		$this->Setting_model->set($this->session->userdata('userID'));
+		$userID = $this->session->userdata('userID');
+		$this->Setting_model->set($userID);
 
 		if ($this->input->server('REQUEST_METHOD') === 'POST') {
-			$field = array();
-			if($this->input->post('email')!=$this->Setting_model->userEmail) {
-				$this->form_validation->set_rules('email','Email','required|valid_email|max_length[75]|is_unique[user.userEmail]');
-				$field['userEmail'] = $this->input->post('email');
-			}
-			if($this->input->post('fname')!=$this->Setting_model->userFirstName) {
-				$this->form_validation->set_rules('fname','First Name','required|min_length[3]|max_length[45]|alpha_int');
-				$field['userFirstName'] = $this->input->post('fname');
-			}
-			if($this->input->post('lname')!=$this->Setting_model->userLastName) {
-				$this->form_validation->set_rules('lname','Last Name','required|min_length[3]|max_length[45]|alpha_int');
-				$field['userLastName'] = $this->input->post('lname');
+			$update = array();
+			$fields = array(
+				'email' => array(
+					'col'   => 'userEmail',
+					'field' => 'email',
+					'label' => 'Email',
+					'rules' => 'required|valid_email|max_length[75]|is_unique[user.userEmail]',
+				),
+				'fname' => array(
+					'col'   => 'userFirstName',
+					'field' => 'fname',
+					'label' => 'First Name',
+					'rules' => 'required|min_length[3]|max_length[45]|alpha_int',
+				),
+				'lname' => array(
+					'col'   => 'userLastName',
+					'field' => 'lname',
+					'label' => 'Last Name',
+					'rules' => 'required|min_length[3]|max_length[45]|alpha_int',
+				),
+			);
+
+			foreach ($fields as $f_name => $field) {
+				if ($this->input->post($f_name) != $this->Setting_model->{$field['col']}) {
+					$this->form_validation->set_rules(array($field));
+					$update[$field['col']] = $this->input->post($f_name);
+				}
 			}
 
-			if (is_null($field)) {
-				$data["alert_message"] = "Enter data you want to update.";
-				$data["alert_class"] = "alert-error";
+			if (empty($update)) {
+				$data['alert_message'] = 'Enter data you want to update.';
+				$data['alert_class'] = 'alert-error';
 			} else {
-				$this->form_validation->set_rules('pwd','Password','required|min_length[8]|max_length[64]|checkpassword');
+				$this->form_validation->set_rules(
+					'pwd',
+					'Password',
+					'required|min_length[8]|max_length[64]|checkpassword'
+				);
 			}
 
-			if($this->form_validation->run() == TRUE) {	
-				if ($this->Setting_model->update_account($field, $this->session->userdata('userID'))) {
-					$data["alert_message"] = "Your data is update succesfuly.";
-					$data["alert_class"] = "alert-success";
-					$this->Setting_model->set($this->session->userdata('userID'));
-					$this->session->set_userdata($field);
+			if ($this->form_validation->run() == true) {
+				if ($this->Setting_model->update_account($update, $userID)) {
+					$data['alert_message'] = 'Your data has been updated successfully.';
+					$data['alert_class'] = 'alert-success';
+					$this->Setting_model->set($userID);
+					$this->session->set_userdata($update);
+					$this->session->set_userdata('userFullName', 
+						$this->Setting_model->userFirstName . ' ' .
+						$this->Setting_model->userLastName
+					);
 				} else {
-					$data["alert_message"] = "Something went wrong.";
-					$data["alert_class"] = "alert-error";	
+					$data['alert_message'] = 'Something went wrong.';
+					$data['alert_class'] = 'alert-error';	
 				}
 			}
 		}
 
-		$data["userEmail"] = $this->Setting_model->userEmail;
-		$data["userFirstName"] = $this->Setting_model->userFirstName;
-		$data["userLastName"] = $this->Setting_model->userLastName;
+		$data['userEmail'] = $this->Setting_model->userEmail;
+		$data['userFirstName'] = $this->Setting_model->userFirstName;
+		$data['userLastName'] = $this->Setting_model->userLastName;
+		$data['fullname'] = $this->session->userdata('userFullName');
 
-		$data["fullname"] = $this->session->userdata('userFullName');
-		$this->load->view('admin/default',$data);
-
+		$this->load->view('admin/default', $data);
 		$this->User_model->admin_logged();
 	}
 
@@ -76,25 +103,50 @@ class Setting extends CI_Controller {
 		$this->load->library('session');
 		$this->load->library('form_validation');
 
-		$data["base_url"] = $this->load->helper(array('form', 'url'));
-		$data["title"] = "UygunCart";
-		$data["breadcrumb"] = array("admin/home"=> "Dashboard", "admin/setting"=> "Setting", "last"=> "Password");
-		$data["fullname"] = $this->session->userdata('userFullName');
-		$data["mainview"] = "password";
+		$data = array(
+			'base_url' => $this->load->helper(array('form', 'url')),
+			'title' => 'UygunCart',
+			'breadcrumb' => array(
+				'admin/home' => 'Dashboard',
+				'admin/setting' => 'Setting',
+				'last' => 'Password'
+			),
+			'fullname' => $this->session->userdata('userFullName'),
+			'mainview' => 'password'
+		);
 
-		$this->form_validation->set_rules('current_password', 'Current Password', 'required|min_length[8]|max_length[64]|checkpassword');
-		$this->form_validation->set_rules('new_password', 'New Password', 'required|min_length[8]|max_length[64]|matches[confirm_password]');
-		$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|min_length[8]|max_length[64]|matches[new_password]');
+		$rules = array(
+			array(
+				'field' => 'current_password',
+				'label' => 'Current Password',
+				'rules' => 'required|min_length[8]|max_length[64]|checkpassword'
+			),
+			array(
+				'field' => 'new_password',
+				'label' => 'New Password',
+				'rules' => 'required|min_length[8]|max_length[64]'
+			),
+			array(
+				'field' => 'confirm_password',
+				'label' => 'Confirm Password',
+				'rules' => 'required|matches[new_password]'
+			)
+		);
+		$this->form_validation->set_rules($rules);
 		
-		if($this->form_validation->run() == TRUE) {
-			if($this->Setting_model->update_account(array('userPassword'=>$this->input->post('new_password')),$this->session->userdata('userID'))) {
-				$data["alert_message"] = "Your data is update succesfuly.";
-				$data["alert_class"] = "alert-success";
+		if ($this->form_validation->run() == true) {
+			$update = array(
+				'userPassword' => $this->input->post('new_password'),
+			);
+			if ($this->Setting_model->update_account($update, $this->session->userdata('userID'))) {
+				$data['alert_message'] = 'Your data is update succesfuly.';
+				$data['alert_class'] = 'alert-success';
 			} else {
-				$data["alert_message"] = "Something went wrong.";
-				$data["alert_class"] = "alert-error";		
+				$data['alert_message'] = 'Something went wrong.';
+				$data['alert_class'] = 'alert-error';		
 			}
 		}
-		$this->load->view('admin/default',$data);
+
+		$this->load->view('admin/default', $data);
 	}
 }
