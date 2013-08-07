d0ec5db96be52d59220806cadc1920902b7fa475
Omer Karadagli <ufokaradagli@gmail.com>
2013-08-07 23:18:38 +0100

    DRY admin home controller, remove test controller

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -1,23 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller 
+class Home extends Admin_Controller
 {
 	public function Index()
 	{
-		$this->load->model('User_model');
-		$this->load->library('session');
-		$this->load->helper('url');
-		$this->User_model->admin_logged();
-
 		$data = array(
 			'title' => 'UygunCart',
 			'breadcrumb' => array(
 				'last' => 'Dashboard'
 			),
-			'mainview' => 'home',
-			'fullname' => $this->session->userdata('userFullName')
+			'mainview' => 'home'
 		);
 
-		$this->load->view('admin/default', $data);
+		$this->load_view($data);
 	}
 }
