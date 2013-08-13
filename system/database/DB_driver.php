4dc08b54f97fc3b79c443ff6bef01677d3c1d7d6
Omer Karadagli <ufokaradagli@gmail.com>
2013-08-13 20:33:20 +0100

    Revert framework system files.

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -30,10 +30,10 @@
  */
 class CI_DB_driver {
 
-	var $username		= 'root';
-	var $password		= '1234';	
-	var $hostname 		= 'localhost';
-	var $Database 		= 'uyguncart';
+	var $username;
+	var $password;
+	var $hostname;
+	var $database;
 	var $dbdriver		= 'mysql';
 	var $dbprefix		= '';
 	var $char_set		= 'utf8';
