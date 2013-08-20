b716cb28650a9144f0f074c178b456a14241d1c1
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-08-20 17:46:43 +0300

    started to make product detail page

--- application/views/product.php
+++ application/views/product.php
@@ -1,3 +1,14 @@
 <div class="panel">
-asd
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-sm-9">
+				<div class="img-main-frame pull-left">
+					
+				</div>
+			</div>
+			<div class="col-sm-3">
+				asaasdkapsodkapsd
+			</div>
+		</div>
+	</div>
 </div>
