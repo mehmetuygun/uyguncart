02b0057d94b44a5632b23642b10f664c04ca23b7
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-08-25 13:53:36 +0300

    product detail

--- application/views/body/header.php
+++ application/views/body/header.php
@@ -17,24 +17,24 @@
                         <a href="#">Login</a>
                     </span>
                 </div>
+            </div>
             <div class="navbar navbar-default navbar-static-top">
-    			<div class="container">
-    				<div class="row">
-    						<div class="col-lg-3">
-    							<a class="navbar-brand" href="#">UygunCart</a>
-    						</div>
-    						<div class="col-lg-9">
-    						<form class="navbar-form pull-left" action="<?php echo base_url('search') ?>" method="get">
-    	              			<input type="text" name="q" class="form-control" id="search" placeholder="Search" value="<?php if(isset($_GET["q"])) echo $_GET["q"]; ?>">
-    	              			<button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i></button>
-    	            		</form>
+                <div class="container">
+                    <div class="row">
+                            <div class="col-lg-3">
+                                <a class="navbar-brand" href="#">UygunCart</a>
+                            </div>
+                            <div class="col-lg-9">
+                            <form class="navbar-form pull-left" action="<?php echo base_url('search') ?>" method="get">
+                                <input type="text" name="q" class="form-control" id="search" placeholder="Search" value="<?php if(isset($_GET["q"])) echo $_GET["q"]; ?>">
+                                <button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i></button>
+                            </form>
                             <span class="navbar-form pull-right">
                                 <a href="#" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span> Empty</a>
                             </span>
-    						</div>
-    					</div>
-    			</div>
-                
+                            </div>
+                        </div>
+                </div>
             </div>
-            </div>
+
 			<div class="container">
