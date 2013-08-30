b2c26f59aa581ea337c2f1bd8b23e1dc7639f44c
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-08-30 18:15:59 +0300

    register page design

--- application/views/register.php
+++ application/views/register.php
@@ -1,4 +1,44 @@
 <div class="panel">
 	<div class="panel-heading"><h3 class="panel-title">Register</h3></div>
-	<div class="panel-body">register</div>
+	<div class="panel-body">
+		<h4>User register form.</h4>
+		<hr>
+		<form class="form-horizontal" role="form">
+		  	<div class="form-group">
+		    	<label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+		    <div class="col-lg-10">
+		      		<input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+		    	</div>
+		  	</div>
+		  	<div class="form-group">
+		    	<label for="inputfirstname" class="col-lg-2 control-label">First Name</label>
+		    <div class="col-lg-10">
+		      		<input type="text" class="form-control" id="inputfirstname" placeholder="First Name">
+		    	</div>
+		  	</div>
+		  	<div class="form-group">
+		    	<label for="inputlastname" class="col-lg-2 control-label">Last Name</label>
+		    <div class="col-lg-10">
+		      		<input type="text" class="form-control" id="inputlastname" placeholder="Email">
+		    	</div>
+		  	</div>
+		  	<div class="form-group">
+		    	<label for="inputPassword1" class="col-lg-2 control-label">Password</label>
+		    	<div class="col-lg-10">
+		      		<input type="password" class="form-control" id="inputPassword1" placeholder="Password">
+		    	</div>
+		  	</div>
+		  	<div class="form-group">
+		    	<label for="inputPassword2" class="col-lg-2 control-label">Re-password</label>
+		    	<div class="col-lg-10">
+		      		<input type="re-password" class="form-control" id="inputPassword2" placeholder="Re-password">
+		    	</div>
+		  	</div>
+		  	<div class="form-group">
+		    	<div class="col-lg-offset-2 col-lg-10">
+		      		<button type="submit" class="btn btn-primary">Create</button>
+		    	</div>
+		  	</div>
+		</form>
+	</div>
 </div>
