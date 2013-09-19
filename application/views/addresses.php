279335ef716764ac60d6fec6f2f8fbfe34b9c03c
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-09-19 22:01:48 +0300

    address model

--- application/views/addresses.php
+++ application/views/addresses.php
@@ -1,18 +1,13 @@
 <div class="panel panel-default">
 	<div class="panel-heading"><h3 class="panel-title">Adresses</h3></div>
 	<div class="panel-body">
-		<?php 
-		if(isset($select)) {
-			echo 'post';
-		} else {
-		?>
-		<h4>Primary Address <small>This is your contact address. Keep it up to date.</small></h4>
-		<p>Test, 123123 sokak</p><button class="btn btn-sm">Create</button>
-		<hr>
-		<h4>Shipping Address <small>This address is your items where to go. Make sure it is accurate.</small></h4>
-		<p>Test, 123123 sokak</p><button class="btn btn-sm">Create</button>
-		<?php 
-		} // end of if
-		?>
+		<table >
+			<thead></thead>
+			<thead>Address</thead>
+			<thead>Action</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
 	</div>
 </div>
