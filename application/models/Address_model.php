3332b44737802bc25b36ae9f4365c9a27475b3b6
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-09-20 22:28:14 +0300

    address model

---
+++ application/models/Address_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Address_model extends MY_Model
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+		parent::initialize('address', 'id');
+	}
+
+	public function fetch(array $params = array())
+	{
+		$this->join = array('country', 'address.id = country_id', 'left');
+		return parent::fetch($params);
+	}
+}
