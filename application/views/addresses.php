0fa0785c8ac417afabb27658e9fe6437ff85c917
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-09-23 18:34:11 +0300

    form of adding new address fixed

--- application/views/addresses.php
+++ application/views/addresses.php
@@ -9,7 +9,7 @@
 		  	<div class="form-group">
 		    	<label for="name" class="col-lg-2 control-label">Full Name:</label>
 		    	<div class="col-lg-10">
-		      		<input type="text" id="name" name="name" class="form-control" value="<?php set_value('name', '5') ?>">
+		      		<input type="text" id="name" name="name" class="form-control" value="<?php echo set_value('name', $fullname); ?>">
 		      		<span class="help-block"><?php echo form_error('name'); ?></span>
 		    	</div>
 		  	</div>
@@ -23,28 +23,28 @@
 		  	<div class="form-group">
 		    	<label for="city" class="col-lg-2 control-label">City:</label>
 		    	<div class="col-lg-10">
-		      		<input type="text" id="city" name="city" class="form-control" value="<?php set_value('city', '') ?>">
+		      		<input type="text" id="city" name="city" class="form-control" value="<?php echo set_value('city', '') ?>">
 		      		<span class="help-block"><?php echo form_error('city'); ?></span>
 		    	</div>
 		  	</div>
 		  	<div class="form-group">
 		    	<label for="city" class="col-lg-2 control-label">Address 1:</label>
 		    	<div class="col-lg-10">
-		      		<input type="text" id="address1" name="address1" class="form-control" value="<?php set_value('address1', '') ?>">
+		      		<input type="text" id="address1" name="address1" class="form-control" value="<?php echo set_value('address1', '') ?>">
 		      		<span class="help-block"><?php echo form_error('address1'); ?></span>
 		    	</div>
 		  	</div>
 		  	<div class="form-group">
 		    	<label for="city" class="col-lg-2 control-label">Address 2:</label>
 		    	<div class="col-lg-10">
-		      		<input type="text" id="address2" name="address2" class="form-control" value="<?php set_value('address2', '') ?>">
+		      		<input type="text" id="address2" name="address2" class="form-control" value="<?php echo set_value('address2', '') ?>">
 		      		<span class="help-block"><?php echo form_error('address2'); ?></span>
 		    	</div>
 		  	</div>
 		  	<div class="form-group">
 		    	<label for="city" class="col-lg-2 control-label">Postcode:</label>
 		    	<div class="col-lg-10">
-		      		<input type="text" id="postcode" name="postcode" class="form-control" value="<?php set_value('postcode', '') ?>">
+		      		<input type="text" id="postcode" name="postcode" class="form-control" value="<?php echo set_value('postcode', '') ?>">
 		      		<span class="help-block"><?php echo form_error('postcode'); ?></span>
 		    	</div>
 		  	</div>
