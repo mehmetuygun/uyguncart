e1b28146b34625e8b6d26f49e991b63ad931c14a
Omer Karadagli <ufokaradagli@gmail.com>
2013-11-11 19:55:05 +0000

    Fix footer stray end tag div

--- application/templates/default/body/footer.php
+++ application/templates/default/body/footer.php
@@ -1,47 +1,46 @@
-					<hr>
-				</div>
-				<div id="push"></div>
-		    	<!-- End of wrap -->
+				<hr>
 			</div>
-			<div id="footer">
-		      	<div class="container">
-		      		<div class="row">
-		      			<div class="col-md-3">
-		      				<h4>Information</h4>
-		      				<ul class="list-unstyled">
-		      					<li><a href="#">About</a></li>
-		      					<li><a href="#">Contact</a></li>
-		      					<li><a href="#">Help</a></li>
-		      				</ul>
-		      			</div>
-		      			<div class="col-md-3">
-		      				<h4>Follow us on</h4>
-		      				<ul class="list-unstyled">
-		      					<li><a href="#">Facebook</a></li>
-		      					<li><a href="#">Twitter</a></li>
-		      					<li><a href="#">Google+</a></li>
-		      				</ul>
-		      			</div>
-		      			<div class="col-md-3">
-		      				<h4>Categories</h4>
-		      				<ul class="list-unstyled">
-		      					<li><a href="#">Software</a></li>
-		      					<li><a href="#">Hardware</a></li>
-		      					<li><a href="#">Electronic</a></li>
-		      					<li><a href="#">Gift</a></li>
-		      				</ul>
-		      			</div>
-		      			<div class="col-md-3">
-		      				<h4>Payment</h4>
-		      				<ul class="list-unstyled">
-		      					<li><a href="#">Payment method</a></li>
-		      				</ul>
-		      			</div>
-		      		</div>
-		      	</div>
-		      	<!-- end of footer -->
-		    </div>
-	    </div>
+			<div id="push"></div>
+			<!-- End of wrap -->
+		</div>
+		<div id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<h4>Information</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">About</a></li>
+							<li><a href="#">Contact</a></li>
+							<li><a href="#">Help</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h4>Follow us on</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">Facebook</a></li>
+							<li><a href="#">Twitter</a></li>
+							<li><a href="#">Google+</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h4>Categories</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">Software</a></li>
+							<li><a href="#">Hardware</a></li>
+							<li><a href="#">Electronic</a></li>
+							<li><a href="#">Gift</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h4>Payment</h4>
+						<ul class="list-unstyled">
+							<li><a href="#">Payment method</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- end of footer -->
+		</div>
 		<script type="text/javascript" src="<?php echo base_url('public/js/jquery-1.10.2.min.js');?>"></script>
 		<script type="text/javascript" src="<?php echo base_url('public/templates/default/js/bootstrap.min.js');?>"></script>
 		<?php
