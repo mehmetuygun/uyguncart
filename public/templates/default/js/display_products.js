9080a27cc85edbf45d2605ac10b53f6f65cb0664
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-11-11 16:53:01 +0200

    display of product made ajax on main page

--- public/templates/default/js/display_products.js
+++ public/templates/default/js/display_products.js
@@ -21,6 +21,17 @@ function select_mode() {
 	return select;
 }
 
+function fileExists(url) {
+    if(url){
+        var req = new XMLHttpRequest();
+        req.open('GET', url, false);
+        req.send();
+        return req.status==200;
+    } else {
+        return false;
+    }
+}
+
 function ajax(select) {
 	$.post('home/ajax', {select: select}, function(data) {
 		display(data);
@@ -29,11 +40,38 @@ function ajax(select) {
 
 function display(data) {
 	
-	var html;
+	var html = '';
+	var img_200_path = 'public/images/200/';
+	var cart_url = base_url+'cart';
 
-	html += '<div class="row f-space">';
+	for(var i=0;i<9;i++) {
+		if(i==0 || i==3 || i==6) {
+			html += '<div class="row f-space">';
+		}
 
-	alert(data[1].product_id);
+		var p_url = base_url + 'product/id/'+ data[i].product_id;
+		var img_src = base_url+'public/images/135/noimage.jpg';
+		if(typeof data[i].product_full_name != 'undefined' && fileExists(img_200_path+data[i].full_name)) {
+			img_src = base_url+img_200_path+data[i].full_name;
+		}
 
-	html += '</div>';
+		html += '<div class="col-lg-4">';
+        html += '<div class="thumbnail">';
+        html += '<a class="thumbnail" href="'+p_url+'">';
+        html += '<img alt="200x150" src="'+img_src+'" /></a>';
+        html += '<div class="caption">';
+        html += '<h4><a href="'+p_url+'">'+data[i].name+'</a></h4>';
+        html += '<h4><span class="price">$'+data[i].price+'</span></h4>';
+        html += '<form action="'+cart_url+'" method="post">';
+        html += '<input type="hidden" value="'+data[i].product_id+'" name="product_id" />';
+        html += '<button type="submit" class="btn btn-danger" style="width:100%">';
+        html += 'Add To Cart';
+        html += '</button></form></div></div></div>';
+
+        if (i==2 || i==5 || i==8) { 
+            html += '</div>';
+        }
+	}
+
+	$('.display_products').html(html);
 }
