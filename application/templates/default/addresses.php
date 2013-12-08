5fff95761ee71c40676dd876d665ad1c217f16c9
Mehmet Uygun <mehmet.uygun@outlook.com>
2013-12-08 18:18:27 +0200

    added html of address modal

--- application/templates/default/addresses.php
+++ application/templates/default/addresses.php
@@ -185,8 +185,80 @@
 				</tbody>
 			</table>
 			<a href="<?php echo base_url('user/addresses/add') ?>" class="btn btn-primary">Add New Address</a>
+			<button class="btn btn-primary" data-toggle="modal" data-target="#addressModal">Add New Address *Modal</button>
 		<?php
 		}
 		?>
 	</div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="addressModal" tabindex="-1" role="dialog" aria-labelledby="addressModalLabel" aria-hidden="true">
+  	<div class="modal-dialog">
+    	<div class="modal-content">
+      	<div class="modal-header">
+        	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        	<h4 class="modal-title" id="addressModalLabel">Modal title</h4>
+      	</div>
+      	<div class="modal-body">
+			<div class="container">
+				<form class="form-horizontal" role="form">
+					<div class="form-group">
+						<label for="inputName" class="col-lg-3 control-label">Address Name:</label>
+						<div class="col-lg-9">
+							<input type="text" id="inputName" name="addressName" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="name" class="col-lg-3 control-label">Full Name:</label>
+						<div class="col-lg-9">
+							<input type="text" id="name" name="name" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="country_id" class="col-lg-3 control-label">Country:</label>
+						<div class="col-lg-9">
+							<?php echo form_dropdown('country_id', $countries, set_value('country_id', $addresses[0]['country_id']), 'class = "form-control" id="country_id"') ?>
+							<span class="help-block"><?php echo form_error('country_id'); ?></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="city" class="col-lg-3 control-label">City:</label>
+						<div class="col-lg-9">
+							<input type="text" id="city" name="city" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="address1" class="col-lg-3 control-label">Address 1:</label>
+						<div class="col-lg-9">
+							<input type="text" id="address1" name="address1" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="address2" class="col-lg-3 control-label">Address 2:</label>
+						<div class="col-lg-9">
+							<input type="text" id="address2" name="address2" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="postcode" class="col-lg-3 control-label">Postcode:</label>
+						<div class="col-lg-9">
+							<input type="text" id="postcode" name="postcode" class="form-control" value="">
+							<span class="help-block"></span>
+						</div>
+					</div>
+				
+			</div>
+      	</div>
+      		<div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        		<button type="button" class="btn btn-primary" id="modalSubmitButton">Save changes</button>
+      		</div>
+      		</form>
+    	</div><!-- /.modal-content -->
+  	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
