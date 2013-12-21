fe323b1098c2b5865e7bb9f205f2012f58a577a0
Omer Karadagli <ufokaradagli@gmail.com>
2013-12-21 19:36:46 +0000

    Fix allignment of buttons in checkout

--- application/templates/default/checkout_address.php
+++ application/templates/default/checkout_address.php
@@ -51,7 +51,7 @@
 			</div>
 		</div>
 		<hr>
-		<a href="<?php echo base_url('cart'); ?>" class="btn btn-primary">Back</a>
+		<a href="<?php echo base_url('cart'); ?>" class="btn btn-primary pull-left">Back</a>
 		<button type="submit" class="btn btn-primary pull-right">Next</button>
 		</form>
 	</div>
