758ed1af086ad5df0170ac7b7e01fb267cac5d45
Omer Karadagli <ufokaradagli@gmail.com>
2013-12-28 15:32:19 +0000

    Fix indentation in address stage of checkout

--- application/templates/default/checkout_address.php
+++ application/templates/default/checkout_address.php
@@ -6,25 +6,25 @@
 				data-target="#addressModal" data-toggle="modal">Add New Address</a>
 		</div>
 		<form method="POST" action="">
-		<div class="row">
-			<div class="col-md-6">
-				<h4>Shipping Address</h4>
-			  	<div class="form-group <?php if(form_error('saddress')) echo 'has-error'; ?>">
-			    	<select id="saddress" name="saddress" class="form-control"></select>
-			  		<span class="help-block"><?php echo form_error('saddress'); ?></span>
-			  	</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h4>Shipping Address</h4>
+					<div class="form-group <?php if(form_error('saddress')) echo 'has-error'; ?>">
+						<select id="saddress" name="saddress" class="form-control"></select>
+						<span class="help-block"><?php echo form_error('saddress'); ?></span>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<h4>Billing Address</h4>
+					<div class="form-group <?php if(form_error('baddress')) echo 'has-error'; ?>">
+						<select id="baddress" name="baddress" class="form-control"></select>
+						<span class="help-block"><?php echo form_error('baddress') ?></span>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<h4>Billing Address</h4>
-				<div class="form-group <?php if(form_error('baddress')) echo 'has-error'; ?>">
-			    	<select id="baddress" name="baddress" class="form-control"></select>
-			  		<span class="help-block"><?php echo form_error('baddress') ?></span>
-			  	</div>
-			</div>
-		</div>
-		<hr>
-		<a href="<?php echo base_url('cart'); ?>" class="btn btn-primary pull-left">Back</a>
-		<button type="submit" class="btn btn-primary pull-right">Next</button>
+			<hr>
+			<a href="<?php echo base_url('cart'); ?>" class="btn btn-primary pull-left">Back</a>
+			<button type="submit" class="btn btn-primary pull-right">Next</button>
 		</form>
 	</div>
 </div>
