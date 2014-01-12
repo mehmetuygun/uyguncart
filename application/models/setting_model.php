a8afbc9727acd566821ccbcb2313e8aa4e5d8e8e
Omer Karadagli <ufokaradagli@gmail.com>
2014-01-12 15:16:42 +0000

    Modify setting model to do upserts

--- application/models/setting_model.php
+++ application/models/setting_model.php
@@ -11,17 +11,31 @@ class Setting_model extends CI_Model
 		$this->load->database();
 	}
 
-	public function get($setting)
+	/**
+	 * Gets the value of specified setting
+	 *
+	 * @param  string $name    Name of the setting
+	 *
+	 * @return string          Value of the setting
+	 */
+	public function get($name)
 	{
 		$row = $this->db
 			->from($this->table)
-			->where('name', $setting)
+			->where('name', $name)
 			->get()
 			->row();
 
 		return $row['value'];
 	}
 
+	/**
+	 * Upserts settings given in the array
+	 *
+	 * @param  array $settings Associative array of settings
+	 *
+	 * @return void
+	 */
 	public function update($settings)
 	{
 		foreach ($settings as $name => $value) {
@@ -29,6 +43,19 @@ class Setting_model extends CI_Model
 			$cond = array('name' => $name);
 
 			$this->db->update($this->table, $data, $cond);
+			if ($this->db->affected_rows()) {
+				continue;
+			}
+
+			// Check whether the setting exists when no rows were affected
+			$exists = $this->db->from($this->table)
+				->where('name', $name)
+				->count_all_results();
+
+			if (!$exists) {
+				$setting_row = array_merge($cond, $data);
+				$this->db->insert($this->table, $setting_row);
+			}
 		}
 	}
 }
