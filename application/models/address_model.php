9d1d8fc267ed2c9a74cf52ffda96824d8351bbaf
Omer Karadagli <ufokaradagli@gmail.com>
2014-02-03 23:52:10 +0000

    Added PHPDoc for address model

--- application/models/address_model.php
+++ application/models/address_model.php
@@ -8,12 +8,26 @@ class Address_model extends MY_Model
 		parent::initialize('address', 'address_id');
 	}
 
+	/**
+	 * Inserts a new address with added_date
+	 *
+	 * @param  array  $params Details of the address
+	 *
+	 * @return integer        ID of the created address
+	 */
 	public function insert(array $params = array())
 	{
 		$params['added_date'] = date('Y-m-d H:i:s');
 		return parent::insert($params);
 	}
 
+	/**
+	 * Deletes an address permanently or temporarly depending if it was used
+	 *
+	 * @param  integer $id ID of the address to be deleted
+	 *
+	 * @return bool        Whether the delete succeeded
+	 */
 	public function delete($id)
 	{
 		if ($this->is_used($id)) {
@@ -23,6 +37,13 @@ class Address_model extends MY_Model
 		return parent::delete($id);
 	}
 
+	/**
+	 * Creates a new address using the provided ID
+	 *
+	 * @param  integer $id ID of the address to be duplicated
+	 *
+	 * @return integer     Insert ID
+	 */
 	public function duplicate($id)
 	{
 		$this->load->database();
@@ -36,12 +57,26 @@ class Address_model extends MY_Model
 		return $this->insert($row);
 	}
 
+	/**
+	 * Soft-deletes an address by setting status to 0
+	 *
+	 * @param  integer $id ID of the address to be soft-deleted
+	 *
+	 * @return bool        Whether the update succeeded
+	 */
 	public function soft_delete($id)
 	{
 		$params['status'] = 0;
 		return parent::update($params, $id);
 	}
 
+	/**
+	 * Check if an address has been used
+	 *
+	 * @param  integer  $address_id ID of the address to be checked
+	 *
+	 * @return boolean              Whether the address is used
+	 */
 	public function is_used($address_id)
 	{
 		$this->load->database();
